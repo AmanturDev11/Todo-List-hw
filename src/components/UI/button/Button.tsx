81c62ae318784005f0175ee5e4e6e48f51256d46
AmanturDev11 <ismailovamantur22@gmail.com>
2024-02-21 15:29:18 +0600

    hw

--- src/components/UI/button/Button.tsx
+++ src/components/UI/button/Button.tsx
@@ -1,11 +1,17 @@
 import scss from "./Button.module.scss";
+import { FC, ReactNode } from "react";
 
-const Button = ({ onClick, children }) => {
+const Button: FC<{
+	onClick: () => void;
+	children: ReactNode;
+}> = ({ onClick, children }) => {
 	return (
 		<div className={scss.Button}>
 			<div className="container">
 				<div className={scss.content}>
-					<button className={scss.button} onClick={onClick}>{children}</button>
+					<button className={scss.button} onClick={onClick}>
+						{children}
+					</button>
 				</div>
 			</div>
 		</div>
--- src/components/UI/input/Input.tsx
+++ src/components/UI/input/Input.tsx
@@ -1,23 +1,29 @@
 import scss from "./Input.module.scss"
+import  { FC } from "react";
 
-const Input = ({ type, value, setData}) => {
+const Input: FC< {
+  type: string;
+  value: string;
+  setData: (value: string) => void;
+}> = ({ type, value, setData }) => {
 	return (
-    <div className={scss.Input}>
-      <div className="container">
-        <div className={scss.content}>
-          <div className={scss.con}>
-      <input
-        type={type}
-        value={value}
-        onChange={(el) => {
-          setData(el.target.value);
-        }}
-      />
-          </div>
-        </div>
-      </div>
-    </div>
+		<div className={scss.Input}>
+			<div className="container">
+				<div className={scss.content}>
+					<div className={scss.con}>
+						<input
+							type={type}
+							value={value}
+							onChange={(el) => {
+								setData(el.target.value);
+							}}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 };
 
 export default Input;
+
